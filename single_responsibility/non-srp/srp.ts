ef3556d7a4da94b6b539b02e407cfa69a77b6d38
manish0502 <manishgoswami495@gmail.com>
2025-06-26 01:10:31 +0530

    changes

--- single_responsibility/non-srp/srp.ts
+++ single_responsibility/non-srp/srp.ts
@@ -42,29 +42,3 @@ Processing payement....
 Payment processes Successfully!..
 Added to accounting system!...
  */
-
- Summary:
-    our current Order class does not fully follow the Single Responsibility Principle (SRP).
-
-Why?
-
-    The class manages products (add, remove, get).
-    It calculates prices.
-    It generates invoices.
-    It processes payments.
-
-SRP says:
-    A class should have only one reason to change (one responsibility).
-    In real-world terms, each of these actions (inventory, accounting, invoicing, payment) would be handled by a different department or service.
-
-To follow SRP:
-
-    Order should only manage order data (products, order info).
-    Product management, price calculation, invoice generation, and payment processing should be handled by separate classes/services.
-
-Conclusion:
-    our class is functional, but for true SRP and maintainability,
-    split these responsibilities into separate classes or services.
-    This makes your code easier to maintain, test, and extend
- 
-*/
