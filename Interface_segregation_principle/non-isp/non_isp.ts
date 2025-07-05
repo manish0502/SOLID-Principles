3326ecf673adda067bb6e579412b5a683e51aa57
manish0502 <manishgoswami495@gmail.com>
2025-07-06 00:32:11 +0530

    Interface Segregation Principle (ISP)

---
+++ Interface_segregation_principle/non-isp/non_isp.ts
@@ -0,0 +1,12 @@
+/**
+ * Interface Segregation Principle (ISP)
+
+Definition:
+   Clients should not be forced to depend on interfaces they do not use.
+
+In Simple Terms:
+    It's better to have several small, specific interfaces than a large, general-purpose one.
+    Classes should only implement methods that are relevant to them.
+ */
+
+    
