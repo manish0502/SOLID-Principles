cc43f01bf4386c94223ff3f24e153c1dc10baf5a
manish0502 <manishgoswami495@gmail.com>
2025-07-06 00:53:22 +0530

    Penguin is forced to implement fly(), which it cannot do.

--- Interface_segregation_principle/non-isp/non_isp.ts
+++ Interface_segregation_principle/non-isp/non_isp.ts
@@ -12,3 +12,39 @@ In Simple Terms:
           Suppose you have a single interface for all birds:
  */
 
+interface Bird {
+   fly():void;
+   swim(): void ;
+   makeSound():void;
+}
+
+class Duck implements Bird {
+   fly() { console.log("Duck flies"); }
+   swim() { console.log("Duck swims"); }
+   makeSound() { console.log("Quack"); }
+}
+
+class Penguinss implements Bird {
+   fly() { throw new Error("Penguin can't fly"); } // Not relevant!
+   swim() { console.log("Penguin swims"); }
+   makeSound() { console.log("Penguin sound"); }
+}
+
+//usage
+
+const duck = new Duck();
+duck.fly();        // Output: Duck flies
+duck.swim();       // Output: Duck swims
+duck.makeSound();  // Output: Quack
+
+const penguin = new Penguinss();
+penguin.swim();       // Output: Penguin swims
+penguin.makeSound();  // Output: Penguin sound
+penguin.fly();        // Throws Error: Penguin can't fly
+
+/**
+ * Problem:
+      Penguin is forced to implement fly(), which it cannot do.
+      This leads to awkward or error-prone code.
+      Solution: Split the interfaces so each class only implements what it needs.
+ */
