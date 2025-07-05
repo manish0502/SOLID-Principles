7ddea81604686f66a8c2e1bf753e22b7dd2090e6
manish0502 <manishgoswami495@gmail.com>
2025-07-05 23:56:08 +0530

    explanation of lsp

--- Liskov_substitution_principle/recommended-lsp/lsp.ts
+++ Liskov_substitution_principle/recommended-lsp/lsp.ts
@@ -47,3 +47,14 @@ letBirdMakeSound(new Penguins()) // Penguins can make sounds
 letBirdFly(new Aparrow()) // Aparrow can fly ,Aparrow can sound
 
 //letBirdFly(new Penguins()) // TypeScript/JS will error: Penguins does not have fly()
+
+/**
+ * Why This Satisfies LSP
+    Any function expecting a BaseBird can accept both Aparrow and Penguins without error.
+    Only birds that can fly are passed to functions expecting flyingBird, so you never get runtime errors for non-flying birds.
+    You have separated the behaviors so subclasses do not have to implement methods they cannot support.
+
+  This is the correct way to model such hierarchies and fully respects the Liskov Substitution Principle.
+
+
+ */
